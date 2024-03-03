aed4e716e93c7686a9ec3f0aa5e100c63297299e
Khaled Bouguila <khaled.bouguila@polytechnique.edu>
2024-03-03 19:44:57 +0100

    version_0

--- ramp-kits/fligh-rating/problem.py
+++ ramp-kits/fligh-rating/problem.py
@@ -7,7 +7,7 @@ import rampwf as rw
 
 problem_title = 'Flight Chronicles - Predicting Flight ratings'
 
-labels =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Predictions = rw.prediction_types.make_multiclass(labels)
 
 workflow = rw.workflows.Classifier()
@@ -20,18 +20,22 @@ score_types = [
     rw.score_types.Accuracy(name="acc", precision=3),
 ]
 
-def get_data(path=".",split = "train"):
+
+def get_data(path=".", split="train"):
     data_df = pd.read_csv(os.path.join(path, "data", split + ".csv"))
     y = np.array(data_df["rating"].astype("int8"))
     X = data_df.drop(columns=["rating"])
     return X, y
 
+
 def get_test_data(path="."):
     return get_data(path, "test")
+
 
 def get_train_data(path="."):
     return get_data(path, "train")
 
+
 def get_cv(X, y):
-    cv = StratifiedKFold(n_splits=3,shuffle=True, random_state=42)
+    cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
     return cv.split(X, y)
--- ramp-kits/fligh-rating/submissions/starting_kit/classifier.py
+++ ramp-kits/fligh-rating/submissions/starting_kit/classifier.py
@@ -4,11 +4,25 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import make_column_transformer
 from sklearn.ensemble import RandomForestClassifier
 
+
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.categorical_cols = ['header','author','date','place','content','aircraft','traveller_type','seat_type','route','date_flown','recommended','trip_verified']
+        self.categorical_cols = ['header',
+                                 'author',
+                                 'date',
+                                 'place',
+                                 'content',
+                                 'aircraft',
+                                 'traveller_type',
+                                 'seat_type',
+                                 'route',
+                                 'date_flown',
+                                 'recommended',
+                                 'trip_verified']
         self.categorical_transformer = make_pipeline(LabelEncoder())
-        self.preprocessor = make_column_transformer((self.categorical_transformer, self.categorical_cols))
+        self.preprocessor = make_column_transformer((
+            self.categorical_transformer,
+            self.categorical_cols))
         self.model = RandomForestClassifier(n_estimators=100, random_state=1)
         self.pipe = make_pipeline(self.preprocessor, self.model)
 
@@ -20,5 +34,3 @@ class Classifier(BaseEstimator):
 
     def predict_proba(self, X):
         return self.pipe.predict_proba(X)
-    
-
